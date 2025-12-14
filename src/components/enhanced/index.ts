bc52fec7e4f4b670f4af0bef919075b0e7753ee0
Sami Hassan <Mohammed Sami Hasan>
2025-12-15 00:29:47 +0600

    chore: remove deprecated root-level files (moved to frontend folder)

--- src/components/enhanced/index.ts
+++
@@ -1,3 +0,0 @@
-export { EnhancedCard } from './EnhancedCard';
-export { EnhancedButton } from './EnhancedButton';
-export { default as AlgorithmSelectorEnhanced } from './AlgorithmSelectorEnhanced';
